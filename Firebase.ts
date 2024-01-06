b6d1fbc936dcf0e17d9af82b5fb263c72bfb767a
Lautaro-Recio <reciolauti@gmail.com>
2024-01-06 11:54:26 -0300

    .env

--- Firebase.ts
+++ Firebase.ts
@@ -1,27 +1,27 @@
 import { initializeApp } from "firebase/app";
-import { getStorage,} from "firebase/storage";
-import {doc, getFirestore, setDoc} from "firebase/firestore";
+import { getStorage, } from "firebase/storage";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
 import { Product } from "./src/Types";
 const firebaseConfig = {
-    apiKey: "AIzaSyBHoMgxgaEs-pP5j8ElO2jWjWjg1BA83cQ",
-    authDomain: "verificador-a1982.firebaseapp.com",
-    projectId: "verificador-a1982",
-    storageBucket: "verificador-a1982.appspot.com",
-    messagingSenderId: "1076524478470",
-    appId: "1:1076524478470:web:5ab5ad595c624188119387"
-  };
+  apiKey: import.meta.env.VITE_REACT_APP_APIKEY,
+  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_RENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_APP_ID
+};
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export const storage = getStorage(app)
 
- 
 
-export const uploadData = async (data: Product[], where:string) => {
+
+export const uploadData = async (data: Product[], where: string) => {
   const collectionRef = doc(db, 'la mediterranea', where);
 
   try {
-    await setDoc(collectionRef, {data});
+    await setDoc(collectionRef, { data });
     console.log('Datos subidos exitosamente');
   } catch (error) {
     console.error('Error al subir datos:', error);
